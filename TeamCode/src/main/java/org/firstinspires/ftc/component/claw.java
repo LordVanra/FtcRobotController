564b21a76ae94eadf711c29ee9177ffe1c929e33
LordVanra <146479458+LordVanra@users.noreply.github.com>
2024-07-28 08:39:38 -0400

    Update claw.java

--- TeamCode/src/main/java/org/firstinspires/ftc/component/claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/component/claw.java
@@ -3,13 +3,10 @@ package org.firstinspires.ftc.teamcode.component;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class Claw{
-
-    Servo claw;
-    double targetPosition;
     
     @Override
     public void init(){
-      claw = hwmap.get(Servo.class(), ¨claw¨);
+        
     }
 
     public void open(){
