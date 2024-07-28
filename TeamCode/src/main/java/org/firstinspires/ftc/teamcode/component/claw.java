77361064f1dcfcdb9079651a59eaa4a60f2defbd
LordVanra <146479458+LordVanra@users.noreply.github.com>
2024-07-28 08:41:04 -0400

    Create claw.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/component/claw.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.component;
+
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class Claw{
+    
+    @Override
+    public void init(){
+        
+    }
+
+    public void open(){
+
+    }
+
+    public void close(){
+
+    }
+
+    public void update(){
+
+    }
+}
