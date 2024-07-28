37e35198b8a61a2d1336ba124b697297e8f69e2d
LordVanra <146479458+LordVanra@users.noreply.github.com>
2024-07-28 08:38:50 -0400

    Remove mecanum

--- TeamCode/src/main/java/org/firstinspires/ftc/teleop/TeleOpParent.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teleop/TeleOpParent.java
@@ -1,12 +1,7 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
-import com.arcrobotics.ftclib.drivebase.MecanumDrive;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
-import org.firstinspires.ftc.teamcode.core.Bot;
-import org.firstinspires.ftc.teamcode.library.DriveStyle;
-
 
 public class TeleOpParent extends LinearOpMode {
 
@@ -16,13 +11,6 @@ public class TeleOpParent extends LinearOpMode {
         GamepadEx driverOp = new GamepadEx(gamepad1);// driver
 
         waitForStart();
-
-        MecanumDrive drive = new MecanumDrive(
-                Bot.frontLeft,
-                Bot.frontRight,
-                Bot.backLeft,
-                Bot.backRight
-        );
 
         while (opModeIsActive()) {
             //code goes here
